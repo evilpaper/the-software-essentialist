7466818951e24b1b438d5f62dafdd5f68879f50d
Pelle Lundgren <pellburt@gmail.com>
2023-08-03 07:50:27 +0200

    [New]: GREEN - 42 returns 'Fizz'.

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,5 +3,6 @@ export function fizzBuzz(number: number) {
   if (number === 5) return "Buzz";
   if (number === 9) return "Fizz";
   if (number === 15) return "FizzBuzz";
+  if (number === 42) return "Fizz";
   return number.toString();
 }
