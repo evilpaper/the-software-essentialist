86abddba18cbf7b16c1305f76b513bc854dd7495
Pelle Lundgren <pellburt@gmail.com>
2023-08-03 07:45:16 +0200

    [New]: GREEN - 9 returns 'Fizz'.

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,7 @@
 export function fizzBuzz(number: number) {
   if (number === 3) return "Fizz";
   if (number === 5) return "Buzz";
+  if (number === 9) return "Fizz";
   if (number === 15) return "FizzBuzz";
   return "";
 }
